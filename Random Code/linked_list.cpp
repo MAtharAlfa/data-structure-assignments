27be56ff7ceb6b2f1ed0108d96bbdf80e36c2316
MAtharAlfa <alfaathar1311@gmail.com>
2026-04-04 13:41:02 +0700

    init: added a folder and its files inside for making random codes

---
+++ Random Code/linked_list.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+
+struct Element
+{
+    std::string member;
+    Element* next;
+};
+
+typedef Element* Pointer; //first element
+
+typedef Pointer Stack; //head or top
+
+std::string peek(Stack top) {
+    return top->member;
+}
+
+bool isEmpty(Stack top) {
+    if (top == nullptr)
+    {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void createStack(Stack& top) {
+    top = nullptr;
+}
+
+void createElement(Pointer& newElement, char datum) {
+    newElement = new Element;
+    newElement->member = datum;
+    newElement->next = nullptr;
+}
+
+void push(Stack& top, Pointer neweElement) {
+    if (isEmpty(top))
+    {
+        top = neweElement;
+    } else {
+        neweElement->next = top;
+        top = neweElement;
+    }
+}
+
+void pop (Stack& top, Pointer& pDel) {
+    if (isEmpty(top))
+    {
+        std::cout << "Stack Underflow";
+        pDel = nullptr;
+    } else if (top->next == nullptr) {
+        pDel = top;
+        top = nullptr;
+        delete pDel;
+    } else {
+        pDel = top;
+        top = top->next;
+        pDel->next = nullptr;
+        delete pDel;
+    }
+}
+
+void traversal(Stack& top) {
+    Pointer pHelp = top;
+
+    if (isEmpty(top))
+    {
+        std::cout << "Stack is empty";
+    } else {
+        std::cout << "Top\n^\n|\n";
+        do
+        {
+            std::cout << pHelp->member;
+            pHelp = pHelp->next;
+
+        } while (pHelp != nullptr);
+    }
+}
+
+int main(){
+    Pointer newElement, pDel;
+    Stack top;
+    std::string kata;
+
+    createStack(top);
+
+    std::cout << "input: ";
+    getline(std::cin, kata);
+
+    for (size_t i = 0; i < kata.size(); i++)
+    {
+        createElement(newElement, kata[i]);
+        push(top, newElement);
+    }
+
+    std::cout << "\n";
+    
+    traversal(top);
+
+    return 0;
+}
